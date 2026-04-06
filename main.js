ce0733ea55ec4de468bd00ad69c29de759211869
Lashuki <robaqidzelasha@yahoo.com>
2026-04-06 14:46:41 +0400

    working_temporary

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+let arr3 = [...arr1, ...arr2]
+console.log(arr3);
+console.log("--------------------"); 
+
+const languages = ["JavaScript", "Node.js", "React"];
+
+const iterator = languages.entries();
+
+console.log(iterator.next().value); // [0, "JavaScript"]
+console.log(iterator.next().value); // [1, "Node.js"]
+console.log(iterator.next().value); // [2, "React"]
+
